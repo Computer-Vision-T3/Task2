c9e043573d258009dd47bdae270690ed84b805a0
sandy-khalil <sandykhalil3322@gmail.com>
2026-03-20 00:54:52 +0200

    Refactor CannyDetector to use static methods
    Updated CannyDetector class to use static methods and added apply function.

--- src/backend/Module1_CannyEdge/CannyDetector.h
+++ src/backend/Module1_CannyEdge/CannyDetector.h
@@ -5,19 +5,20 @@
 
 class CannyDetector {
 public:
-    cv::Mat sobel(const cv::Mat& image, int kernel_size = 3);
-    cv::Mat roberts(const cv::Mat& image);
-    cv::Mat prewitt(const cv::Mat& image);
-    cv::Mat canny(const cv::Mat& image, int low, int high);
+    // This is the main function expected by your project architecture
+    static cv::Mat apply(cv::Mat img, int lowThresh, int highThresh);
 
-    cv::Mat cannyHandmade(const cv::Mat& image, int low, int high);
+    static cv::Mat sobel(const cv::Mat& image, int kernel_size = 3);
+    static cv::Mat roberts(const cv::Mat& image);
+    static cv::Mat prewitt(const cv::Mat& image);
+    static cv::Mat canny(const cv::Mat& image, int low, int high);
+    static cv::Mat cannyHandmade(const cv::Mat& image, int low, int high);
 
 private:
-    void sobelGradients(const cv::Mat& image, cv::Mat& magnitude, cv::Mat& angle);
-    cv::Mat nonMaxSuppression(const cv::Mat& mag, const cv::Mat& angle);
-    cv::Mat hysteresis(const cv::Mat& img, int low, int high);
-
-    cv::Mat normalize(const cv::Mat& img);
+    static void sobelGradients(const cv::Mat& image, cv::Mat& magnitude, cv::Mat& angle);
+    static cv::Mat nonMaxSuppression(const cv::Mat& mag, const cv::Mat& angle);
+    static cv::Mat hysteresis(const cv::Mat& img, int low, int high);
+    static cv::Mat normalizeInternal(const cv::Mat& img); // Renamed to avoid collision
 };
 
 #endif
