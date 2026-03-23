c7f450e0f744201bfed227e0a26a4033f35dde10
sandy-khalil <sandykhalil3322@gmail.com>
2026-03-23 04:40:09 +0200

    Refactor ParameterBox.h to improve structure and clarity

--- src/frontend/components/ParameterBox.h
+++ src/frontend/components/ParameterBox.h
@@ -2,26 +2,32 @@
 #define PARAMETERBOX_H
 
 #include <QWidget>
+#include <QGridLayout>
+#include <QLabel>
 #include <QSpinBox>
 #include <QSlider>
-#include <QComboBox>
-#include <QLabel>
-#include <QGridLayout>
+#include <QPushButton>
 
 class ParameterBox : public QWidget {
     Q_OBJECT
-
 public:
     explicit ParameterBox(QWidget *parent = nullptr);
     void updateForTask(int taskIndex);
 
+signals:
+    // This signal tells the controller which "Add" button was clicked
+    void quickActionRequested(int taskIndex);
+
 private:
     QGridLayout* layout;
-    
-    // Helper UI builders to keep the code clean
     void clearLayout();
-    void addSpinBox(const QString& id, const QString& label, int min, int max, int defaultVal, int row, int col);
-    void addSlider(const QString& id, const QString& label, int min, int max, int defaultVal, int row, int col, bool isFloat = false);
+    
+    // Helper declarations
+    void addSpinBox(const QString& id, const QString& labelText, int min, int max, int defaultVal, int row, int col);
+    void addSlider(const QString& id, const QString& labelText, int min, int max, int defaultVal, int row, int col, bool isFloat);
+    
+    // FIXED: Added this declaration so ParameterBox.cpp can find it
+    void addButton(const QString& text, int targetTask, int row, int col);
 };
 
-#endif // PARAMETERBOX_H
+#endif
